dfb4ec6f752272f37d4d9a1acff5db76860cc7a4
ander384 <ander384@mail.chapman.edu>
2019-08-26 21:33:07 -0700

    set up password protection

--- app/src/main/java/com/christine/vanishingportrait/MainActivity.java
+++ app/src/main/java/com/christine/vanishingportrait/MainActivity.java
@@ -1,13 +1,25 @@
 package com.christine.vanishingportrait;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView inputDateTextView;
+    private Button enterButton;
+    private Button clearButton;
     ImageView currentPortrait;
     int count = 1;
     @Override
@@ -15,17 +27,88 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        this.inputDateTextView = findViewById(R.id.tv_input_date);
+        this.enterButton = findViewById(R.id.btn_enter);
+        this.clearButton = findViewById(R.id.btn_clear);
+
+        //Forces the tablet to not auto-lock
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
         currentPortrait = findViewById(R.id.iv_current_portrait);
+
+
+        this.enterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Button button = (Button) view;
+                String number = inputDateTextView.getText().toString();
+                if (Objects.equals(number, "EVANESCO")) {
+                    if (count%2 == 1){
+                        currentPortrait.setImageResource(R.drawable.empty_portrait);
+                        ++count;
+                    }
+                    else{
+                        currentPortrait.setImageResource(R.drawable.dumbledore_with_black_background);
+                        ++count;
+                    }/*Intent myIntent = new Intent(ErrorScreen.this, ClueScreen.class);
+                ErrorScreen.this.startActivity(myIntent);*/
+                }
+                else {
+                    inputDateTextView.setTextColor(Color.parseColor("#ff0000"));
+                    Animation anim = new AlphaAnimation(0.0f, 1.0f);
+                    anim.setDuration(50); //You can manage the time of the blink with this parameter
+                    anim.setStartOffset(20);
+                    anim.setRepeatCount(5);
+                    anim.setAnimationListener(new Animation.AnimationListener() {
+                        @Override
+                        public void onAnimationStart(Animation animation) {
+
+                        }
+
+                        @Override
+                        public void onAnimationEnd(Animation animation) {
+                            inputDateTextView.setText("");
+                            inputDateTextView.setTextColor(Color.parseColor("#ffffff"));
+                        }
+
+                        @Override
+                        public void onAnimationRepeat(Animation animation) {
+
+                        }
+                    });
+                    inputDateTextView.startAnimation(anim);
+
+                }
+            }
+        });
+
+        this.clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                inputDateTextView.setText("");
+            }
+        });
+
+
     }
 
     public void onVanish(View view) {
-        if (count%2 == 1){
-            currentPortrait.setImageResource(R.drawable.empty_portrait);
-            ++count;
+        String currentText = inputDateTextView.getText().toString();
+        //Will only concatenate the next number if less than 4 numbers have been entered
+        if (currentText.length()<10)
+        {
+            Button button = (Button) view;
+            String number = button.getText().toString();
+            currentText = currentText + number;
+            inputDateTextView.setText(currentText);
         }
-        else{
-            currentPortrait.setImageResource(R.drawable.dumbledore_with_black_background);
-            ++count;
+        else
+        {
+            Animation anim = new AlphaAnimation(0.0f, 1.0f);
+            anim.setDuration(50); //You can manage the time of the blink with this parameter
+            anim.setStartOffset(20);
+            anim.setRepeatCount(2);
+            inputDateTextView.startAnimation(anim);
         }
     }
 }
